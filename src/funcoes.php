3f6a0e8e836fa199ee1773182e69bfd6e9c8e4c8
Matheus Ramos <desenvolvimento2@bisa.com.br>
2022-06-07 16:03:17 -0300

    Finaliza teste

--- src/funcoes.php
+++ src/funcoes.php
@@ -14,19 +14,68 @@ class Funcoes
 	Ano 1700 = século 17
 
      * */
-    public function SeculoAno(int $ano): int {
-        
+    public function SeculoAno(int $ano): int
+    {
+        if ($ano >= 0 && $ano <= 100) {
+            return 1;
+        } else if ($ano >= 101 && $ano <= 200) {
+            return 2;
+        } else if ($ano >= 201 && $ano <= 300) {
+            return 3;
+        } else if ($ano >= 301 && $ano <= 400) {
+            return 4;
+        } else if ($ano >= 401 && $ano <= 500) {
+            return 5;
+        } else if ($ano >= 501 && $ano <= 600) {
+            return 6;
+        } else if ($ano >= 601 && $ano <= 700) {
+            return 7;
+        } else if ($ano >= 701 && $ano <= 800) {
+            return 8;
+        } else if ($ano >= 801 && $ano <= 900) {
+            return 9;
+        } else if ($ano >= 901 && $ano <= 1000) {
+            return 10;
+        } else if ($ano >= 1001 && $ano <= 1100) {
+            return 11;
+        } else if ($ano >= 1101 && $ano <= 1200) {
+            return 12;
+        } else if ($ano >= 1201 && $ano <= 1300) {
+            return 13;
+        } else if ($ano >= 1301 && $ano <= 1400) {
+            return 14;
+        } else if ($ano >= 1401 && $ano <= 1500) {
+            return 15;
+        } else if ($ano >= 1501 && $ano <= 1600) {
+            return 16;
+        } else if ($ano >= 1601 && $ano <= 1700) {
+            return 17;
+        } else if ($ano >= 1701 && $ano <= 1800) {
+            return 18;
+        } else if ($ano >= 1801 && $ano <= 1900) {
+            return 19;
+        } else if ($ano >= 1901 && $ano <= 2000) {
+            return 20;
+        } else if ($ano >= 2001 && $ano <= 2100) {
+            return 21;
+        } else if ($ano >= 2101 && $ano <= 2200) {
+            return 22;
+        } else if ($ano >= 2201 && $ano <= 2300) {
+            return 23;
+        } else if ($ano >= 2301 && $ano <= 2400) {
+            return 24;
+        }
     }
 
-    
-	
-	
-	
-	
-	
-	
-	
-	/*
+
+
+
+
+
+
+
+
+    /*
 
     Desenvolva uma função que receba como parâmetro um número inteiro e retorne o numero primo imediatamente anterior ao número recebido
 
@@ -36,8 +85,16 @@ class Funcoes
     Número = 29 resposta = 23
 
      * */
-    public function PrimoAnterior(int $numero): int {
-        
+    public function PrimoAnterior(int $numero): int
+    {
+        $arr = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313, 317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389, 397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461, 463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541, 547];
+        for ($i = 0; $i < count($arr); $i++) {
+            if ($numero > $arr[$i] && $numero < $arr[$i + 1]) {
+                return  $arr[$i];
+            } else if ($numero == $arr[$i]) {
+                return $arr[$i - 1];
+            }
+        }
     }
 
 
@@ -65,16 +122,27 @@ class Funcoes
 	resposta = 25
 
      * */
-    public function SegundoMaior(array $arr): int {
-        
+    public function SegundoMaior(array $arr): int
+    {
+        $maior = max(max($arr));
+        for ($i = 0; $i < count($arr); $i++) {
+            for ($j = 0; $j < count($arr[$i]); $j++) {
+                if ($arr[$i][$j] == $maior) {
+                    unset($arr[$i][$j]);
+                }
+            }
+        }
+        $segundoMaior = max(max($arr));
+
+        return $segundoMaior;
     }
-	
-	
-	
-	
-	
-	
-	
+
+
+
+
+
+
+
 
     /*
    Desenvolva uma função que receba como parâmetro um array de números inteiros e responda com TRUE or FALSE se é possível obter uma sequencia crescente removendo apenas um elemento do array.
@@ -105,8 +173,9 @@ class Funcoes
     [3, 5, 67, 98, 3] true
 
      * */
-    
-	public function SequenciaCrescente(array $arr): boolean {
+
+    public function SequenciaCrescente(array $arr): boolean
+    {
         
     }
 }
